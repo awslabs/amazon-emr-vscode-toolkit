640af8e88925e068369f124c12403ba3b72c70e2
Damon P. Cortesi <d.lifehacker@gmail.com>
2022-04-14 16:12:55 -0700

    Add actual instances

--- src/emr_containers.ts
+++ src/emr_containers.ts
@@ -1,6 +1,4 @@
 import * as vscode from "vscode";
-import * as fs from "fs";
-import * as path from "path";
 import {
   EMRContainersClient,
   ListVirtualClustersCommand,
--- src/emr_explorer.ts
+++ src/emr_explorer.ts
@@ -1,14 +1,22 @@
 import * as vscode from "vscode";
-import * as fs from "fs";
-import * as path from "path";
 import {
   EMR,
   ListClustersCommand,
   ClusterState,
-  ListClustersInput,
   DescribeClusterCommand,
-  DescribeClusterCommandInput,
   Application,
+  ClusterSummary,
+  InstanceCollectionType,
+  ListInstanceGroupsCommand,
+  ListInstanceFleetsCommand,
+  InstanceFleetStatus,
+  Cluster,
+  ListInstancesCommand,
+  InstanceTypeSpecification,
+  InstanceGroupType,
+  InstanceStateChangeReason,
+  InstanceState,
+  InstanceFleetType,
 } from "@aws-sdk/client-emr";
 
 export class NodeDependenciesProvider
@@ -44,8 +52,8 @@ export class NodeDependenciesProvider
 
   private async listEMRClusters(client: EMR): Promise<EMRCluster[]> {
     // Currently only show running or waiting clusters
-    // eslint-disable-next-line @typescript-eslint/naming-convention
     const params = {
+      // eslint-disable-next-line @typescript-eslint/naming-convention
       ClusterStates: [ClusterState.RUNNING, ClusterState.WAITING],
     };
     vscode.window.showInformationMessage(
@@ -55,8 +63,7 @@ export class NodeDependenciesProvider
       const result = await client.send(new ListClustersCommand(params));
       return (result.Clusters || []).map((cluster) => {
         return new EMRCluster(
-          cluster.Name || "",
-          cluster.Id || "",
+          cluster,
           this.emrClient,
           vscode.TreeItemCollapsibleState.Collapsed
         );
@@ -71,27 +78,30 @@ export class NodeDependenciesProvider
 
 class EMRCluster extends vscode.TreeItem {
   constructor(
-    public readonly name: string,
-    public readonly id: string,
+    private readonly details: ClusterSummary,
     private readonly emr: EMR,
     public readonly collapsibleState: vscode.TreeItemCollapsibleState
   ) {
-    super(name, collapsibleState);
-    this.tooltip = `${this.name} (${this.id})`;
-    this.description = this.id;
+    super(details.Name || "No name", collapsibleState);
+    this.tooltip = `${details.Name} (${details.Id})`;
+    this.description = details.Id;
   }
 
   public async getChildren(element?: EMRCluster): Promise<vscode.TreeItem[]> {
-    // eslint-disable-next-line @typescript-eslint/naming-convention
     const response = await this.emr.send(
-      new DescribeClusterCommand({ ClusterId: this.id })
+      // eslint-disable-next-line @typescript-eslint/naming-convention
+      new DescribeClusterCommand({ ClusterId: this.details.Id })
     );
+    // TODO (2022-04-13): ERROR CHECKING!
+    console.log(response);
     return [
       new EMRClusterApps(
         this.emr,
         response.Cluster ? response.Cluster.Applications : undefined
       ),
-      new EMRClusterInstances(this.emr),
+      new EMRClusterInstances(
+        this.emr, response.Cluster!,
+      ),
     ];
   }
 }
@@ -114,23 +124,64 @@ class EMRClusterApps extends vscode.TreeItem {
 }
 
 class EMRClusterInstances extends vscode.TreeItem {
-  constructor(private readonly emr: EMR) {
+  constructor(
+    private readonly emr: EMR,
+    private readonly cluster: Cluster,
+  ) {
     super("Instances", vscode.TreeItemCollapsibleState.Collapsed);
+
+    this.cluster = cluster;
   }
 
-  getTreeItem(element: EMRClusterApps): vscode.TreeItem {
+  getTreeItem(element: EMRClusterInstances): vscode.TreeItem {
     return element;
   }
 
-  getChildren(): vscode.TreeItem[] {
+  public async getChildren(element?: EMRClusterInstances|undefined): Promise<vscode.TreeItem[]> {
+    // TODO (2022-04-13): Pagination
+    let instanceCollectionMapping: Map<string, string[]> = new Map();
+    
+    if (this.cluster.InstanceCollectionType === InstanceCollectionType.INSTANCE_GROUP) {
+      const response = await this.emr.send(
+        new ListInstanceGroupsCommand({ ClusterId: this.cluster.Id })
+      );
+      instanceCollectionMapping.set("master", response.InstanceGroups?.filter(item => item.InstanceGroupType === InstanceGroupType.MASTER).map(item => item.Id as string) || []);
+      instanceCollectionMapping.set("core", response.InstanceGroups?.filter(item => item.InstanceGroupType === InstanceGroupType.CORE).map(item => item.Id as string) || []);
+      instanceCollectionMapping.set("task", response.InstanceGroups?.filter(item => item.InstanceGroupType === InstanceGroupType.TASK).map(item => item.Id as string) || []);
+      
+    } else if (
+      this.cluster.InstanceCollectionType === InstanceCollectionType.INSTANCE_FLEET
+    ) {
+      const response = await this.emr.send(
+        new ListInstanceFleetsCommand({ ClusterId: this.cluster.Id })
+      );
+      instanceCollectionMapping.set("master", response.InstanceFleets?.filter(item => item.InstanceFleetType === InstanceFleetType.MASTER).map(item => item.Id as string) || []);
+      instanceCollectionMapping.set("core", response.InstanceFleets?.filter(item => item.InstanceFleetType === InstanceGroupType.CORE).map(item => item.Id as string) || []);
+      instanceCollectionMapping.set("task", response.InstanceFleets?.filter(item => item.InstanceFleetType === InstanceGroupType.TASK).map(item => item.Id as string) || []);
+    }
+
+    const instances = await this.emr.send(
+      new ListInstancesCommand({ClusterId: this.cluster.Id, InstanceStates: [InstanceState.RUNNING, InstanceState.BOOTSTRAPPING, InstanceState.PROVISIONING]})
+    );
+
+    const instanceTypeMapping = {
+      master: instances.Instances?.filter(item => instanceCollectionMapping.get("master")?.includes(item.InstanceGroupId!) ),
+      core: instances.Instances?.filter(item => instanceCollectionMapping.get("core")?.includes(item.InstanceGroupId!)),
+      task: instances.Instances?.filter(item => instanceCollectionMapping.get("task")?.includes(item.InstanceGroupId!)),
+    };
+
     return [
-      new vscode.TreeItem(
-        "Master - r5.4xl",
-        vscode.TreeItemCollapsibleState.None
+      new InstanceNodeTree(
+        "Primary",
+        instanceTypeMapping.master?.map(item => new InstanceNodeTree(item.Ec2InstanceId!, undefined, item.InstanceType)),
       ),
-      new vscode.TreeItem(
-        "Core - 2 r5.8xl",
-        vscode.TreeItemCollapsibleState.None
+      new InstanceNodeTree(
+        "Core",
+        instanceTypeMapping.core?.map(item => new InstanceNodeTree(item.Ec2InstanceId!, undefined, item.InstanceType)),
+      ),
+      new InstanceNodeTree(
+        "Task",
+        instanceTypeMapping.task?.map(item => new InstanceNodeTree(item.Ec2InstanceId!, undefined, item.InstanceType)),
       ),
     ];
   }
@@ -141,4 +192,19 @@ class EMRAppNode extends vscode.TreeItem {
     super(app.Name || "Unknown");
     this.description = app.Version;
   }
+}
+
+class InstanceNodeTree extends vscode.TreeItem {
+  children: InstanceNodeTree[]|undefined;
+
+  constructor(label: string, children?: InstanceNodeTree[], description?: string) {
+    super(label, children === undefined ? vscode.TreeItemCollapsibleState.None : vscode.TreeItemCollapsibleState.Collapsed)
+    this.children = children;
+    if (description) { this.description = description;}
+  }
+
+  getChildren(element?: InstanceNodeTree): InstanceNodeTree[] {
+    return this.children || [];
+  }
+
 }
