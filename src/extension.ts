64cb520858f70aa204aa3974917c1d52e61c4b38
Damon P. Cortesi <d.lifehacker@gmail.com>
2022-07-26 11:44:02 -0700

    Remove the hello world code 👋

--- src/extension.ts
+++ src/extension.ts
@@ -26,8 +26,7 @@ export interface Globals {
   selectedProfile: string;
 }
 const globals = {} as Globals;
-export {globals};
-
+export { globals };
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -148,20 +147,6 @@ export function activate(context: vscode.ExtensionContext) {
   // Use the console to output diagnostic information (console.log) and errors (console.error)
   // This line of code will only be executed once when your extension is activated
   console.log('Congratulations, your extension "emr-tools-v2" is now active!');
-
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
-  let disposable = vscode.commands.registerCommand(
-    "emr-tools-v2.helloWorld",
-    () => {
-      // The code you place here will be executed every time your command is executed
-      // Display a message box to the user
-      vscode.window.showInformationMessage("Hello World from emr-tools!");
-    }
-  );
-
-  context.subscriptions.push(disposable);
 }
 // this method is called when your extension is deactivated
 export function deactivate() {}
